6896b200ee2c825788dfc50409116c2ff3cdd81d
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2026-06-13 17:58:10 +0500

    Add check on node env for correct env integration on this value missing time

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -35,6 +35,9 @@ function parseRedisConnection() {
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
         DATABASE_URL: Joi.string().required(),
         PORT: Joi.number().default(3001),
         JWT_SECRET: Joi.string().min(32).required(),
